d8fba87a4078b186d7e8e36aa189891daff7cdba
retro064 <zapata_giorgio@proton.me>
2024-02-17 17:21:50 -0800

    Update player movement

--- Scout and the Factory/Assets/Scripts/PlayerMovement.cs
+++ Scout and the Factory/Assets/Scripts/PlayerMovement.cs
@@ -1,23 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController controller;
+    private Transform groundCheck;
+    public Transform cam;
     public float speed = 5.0f;
     public float jumpHeight = 10.0f;
-    private float gravity = -9.81f;
-    private bool isGrounded;
+    private const float GRAVITY = -9.81f*2f;
+    private bool grounded;
+    private int jumpNumMax = 2;
+    private int jumpNum;
     private Vector3 velocity;
-    public Transform groundCheck;
-    public LayerMask groundMask;
+    private LayerMask groundMask;
     private float distanceRadius = 0.2f;
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        groundCheck = GameObject.Find("GroundCheck").GetComponent<Transform>();
+        groundMask = LayerMask.GetMask("Ground");
+        Cursor.lockState = CursorLockMode.Locked;
+        
+        jumpNum = jumpNumMax;
     }
 
     // Update is called once per frame
@@ -25,22 +36,39 @@ public class PlayerMovement : MonoBehaviour
     {
         // directional input
         float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
-        controller.Move(direction * speed * Time.deltaTime);
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if (Input.GetButtonDown("Jump") & isGrounded)
+        if (direction.magnitude >= 0.1f)
         {
-            Debug.Log("Jump!");
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
 
-        isGrounded = Physics.CheckSphere(groundCheck.position, distanceRadius, groundMask);
-    }
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * GRAVITY);
+            jumpNum--;
+        } else if (Input.GetButtonDown("Jump") && jumpNum >= 1)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * GRAVITY);
+            jumpNum--;
+        }
 
-    private void FixedUpdate()
-    {
-        velocity.y += gravity * Time.fixedDeltaTime;
-        controller.Move(velocity * Time.fixedDeltaTime);
+        velocity.y += GRAVITY * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if (grounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+            jumpNum = jumpNumMax;
+        }
+
+        grounded = Physics.CheckSphere(groundCheck.position, distanceRadius, groundMask);
     }
 }
